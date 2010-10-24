089ae4d41f2febdc7be020d14722be05537ae6a5
David Hall <david.hall@tv4.se>
2010-10-24 05:18:04 +0200

    Use action to insert footer. Add div class.

--- h3info.php
+++ h3info.php
@@ -4,7 +4,7 @@ Plugin Name: H3info
 Plugin URI: http://www.moonhouse.se/posts/h3info
 Description: Explanatory info from Wikipedia
 Author: David Hall
-Version: 0.0.2
+Version: 0.0.3
 Author URI: http://www.dpg.se
 */
 
@@ -14,35 +14,47 @@ if (!class_exists('H3info')) {
     {
             function H3info() {
             //$this->read_options();
-            //$this->actions_filters();
+            $this->actions_filters();
+        }
+        
+        function actions_filters() {
+        add_filter ('the_content', array ( &$this,'insertFootNote'));
+
         }
         
         function test($article_id='IKEA', $lang='en') {
+        
+        if (false === ($infoboxes = get_transient("h3:$article_id:$lang"))) {
+
          $ch = curl_init();
 
-    // set URL and other appropriate options
     curl_setopt($ch, CURLOPT_URL, "http://dbpedia.org/data/$article_id.json");
     curl_setopt($ch,CURLOPT_HTTPHEADER, array ("Content-Type: application/json;charset=utf-8","Accept: application/json"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
 
-    // grab URL and store it
     $json = curl_exec($ch);
 
-    // close cURL resource, and free up system resources
     curl_close($ch);
 
-    //decode json array
     $json = json_decode($json); 
 
-//print_r($json->{"http://dbpedia.org/resource/IKEA"}->{"http://dbpedia.org/ontology/abstract"});
-        
+        $infoboxes = '';
         foreach ($json->{"http://dbpedia.org/resource/$article_id"}->{"http://dbpedia.org/ontology/abstract"} as $value) {
         	if($value->lang == $lang) {
-		        print '<div>'.$value->value."</div> ";	
+		        $infoboxes.= '<div class="h3-infobox">'.$value->value."</div>  ";	
         	}
         }
+        set_transient("h3:$article_id:$lang", $infoboxes, 60*60*12); 
         }
+        return $infoboxes;
+        }
+        
+        function insertFootNote($content) {
+		$content .= $this->test('H%26M','sv'); 
+        return $content;
+}
+
     
         }
 
